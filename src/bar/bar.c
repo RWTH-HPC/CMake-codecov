c7d4d645b1688356d964e67c151a74864426ccd6
Alexander Haase <alexander.haase@rwth-aachen.de>
2015-06-15 02:43:41 +0200

    initial commit

---
+++ src/bar/bar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "foo.h"
+
+
+int
+main ()
+{
+	printf("%d\n", foo());
+
+	return 0;
+}
---
+++ src/libfoo/foo.c
@@ -0,0 +1,11 @@
+#include "foo.h"
+
+int
+foo ()
+{
+	int i = 0;
+	while (i < 10)
+		i++;
+
+	return i;
+}
