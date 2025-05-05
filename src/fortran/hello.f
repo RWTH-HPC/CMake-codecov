80415c4bb36984501f4ae337ccc18e36718439c9
Simon Schwitanski <schwitanski@users.noreply.github.com>
2025-05-05 08:24:09 +0200

    Clean up licenses (#49)

--- src/fortran/hello.f
+++ src/fortran/hello.f
@@ -1,12 +1,8 @@
 C This file is part of CMake-codecov.
+C SPDX-FileCopyrightText: RWTH Aachen University, Federal Republic of Germany
+C SPDX-FileContributor: Alexander Haase, alexander.haase@rwth-aachen.de
 C
-C Copyright (c)
-C   2015-2020 RWTH Aachen University, Federal Republic of Germany
-C
-C See the LICENSE file in the package base directory for details
-C
-C Written by Alexander Haase, alexander.haase@rwth-aachen.de
-C
+C SPDX-License-Identifier: BSD-3-Clause
 
        program hello
           print *, "Hello World!"
